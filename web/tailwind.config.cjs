a8a382012743f77aee5a145d5f6bc6445672e271
Vitor Chagas <vitormchagas@gmail.com>
2022-09-18 02:32:42 -0300

    Aula 2

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('./background-galaxy@2x.png')",
+        'nlw-gradient': 'linear-gradient(80deg, #9572FC 27%, #43E7AD 41%, #E1D55D 33.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
+  },
+  plugins: [],
+}
