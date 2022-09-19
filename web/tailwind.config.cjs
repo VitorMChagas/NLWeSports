730de70b43d8d4e6b5d81b1b5a420c8a4fbdcfc9
Vitor Chagas <vitormchagas@gmail.com>
2022-09-19 02:22:24 -0300

    Aula 3

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        galaxy: "url('./background-galaxy@2x.png')",
+        galaxy: "url('./background-galaxy.svg')",
         'nlw-gradient': 'linear-gradient(80deg, #9572FC 27%, #43E7AD 41%, #E1D55D 33.57%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
